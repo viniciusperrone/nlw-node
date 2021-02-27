964fc43327d55910fe5a550a4248c26a3b411e41
Vinicius Perrone <perronevinicius2018@gmail.com>
2021-02-27 11:41:50 -0400

    creating router to sendMail

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,13 +2,17 @@ import { Router } from 'express';
 
 import { UserController } from '../database/controllers/UserController';
 import { SurveyController } from '../database/controllers/SurveyController';
+import { SendMailController } from '../database/controllers/SendMailController';
 const router = Router();
 
 const userController = new UserController();
 const surveyController = new SurveyController();
+const sendMailController = new SendMailController();
 
-router.post('/users', userController.create)
-router.post('/surveys', surveyController.create)
-router.get('/surveys/show', surveyController.show)
+router.post('/users', userController.create);
+router.post('/surveys', surveyController.create);
+router.get('/surveys', surveyController.show);
+
+router.post('/sendMail', sendMailController.execute);
 
 export default router;
